ecf331a5aa6334ec720b141bfeadbebd2d5ca9e2
Arlindo Silva <arlindovicente@gmail.com>
2023-10-29 22:28:25 -0300

    home and service
    implementação do home e o serviço com a api do OpenWeatherMap.

---
+++ src/app/modules/wheater/services/weather.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+/**
+ * Serviço responsável por buscar informações de clima de uma cidade através da API do OpenWeatherMap.
+ */
+export class WeatherService {
+  private apiKey = '6382bcc26479515eedc5eecb7474992f';
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Busca informações de clima de uma cidade através da API do OpenWeatherMap.
+   * @param cityName Nome da cidade a ser buscada.
+   * @returns Um Observable com as informações de clima da cidade.
+   */
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
+}
