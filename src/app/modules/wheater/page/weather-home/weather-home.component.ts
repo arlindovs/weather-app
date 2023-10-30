d90c1efcbe8a0294fce2b657d1d7d381cabc703d
Arlindo Silva <arlindovicente@gmail.com>
2023-10-29 22:56:46 -0300

    interface
    interface para listar os campos retornados da api.

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
 
+/**
+ * Componente responsável por exibir as informações climáticas de uma cidade.
+ */
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  /**
+   * Nome da cidade inicial a ser exibida.
+   */
+  initialCityName = 'Itumbiara';
 
+  /**
+   * Dados climáticos da cidade.
+   */
+  weatherDatas!: WeatherDatas;
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  /**
+   * Obtém os dados climáticos da cidade informada.
+   * @param cityName Nome da cidade.
+   */
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName).subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+        console.log(this.weatherDatas);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
