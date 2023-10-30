c3fa22027b95b3bdbe7e6145977c6d5fb3d55b25
Arlindo Silva <arlindovicente@gmail.com>
2023-10-29 23:37:25 -0300

    busca
    criação do campo de busca e impressão do resulta no console.

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 /**
  * Componente responsável por exibir as informações climáticas de uma cidade.
@@ -10,7 +12,11 @@ import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  /**
+   * Utiliza o Subject `destroy$` para gerenciar a destruição do componente. Evitar Memory Leaks.
+   */
+  private readonly destroy$: Subject<void> = new Subject<void>();
   /**
    * Nome da cidade inicial a ser exibida.
    */
@@ -21,25 +27,50 @@ export class WeatherHomeComponent implements OnInit {
    */
   weatherDatas!: WeatherDatas;
 
+  searchIcon = faMagnifyingGlass;
+
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
     this.getWeatherDatas(this.initialCityName);
   }
 
+
   /**
-   * Obtém os dados climáticos da cidade informada.
-   * @param cityName Nome da cidade.
+   * Obtém os dados do clima para uma cidade específica.
+   * @param cityName O nome da cidade para a qual se deseja obter os dados do clima.
+   * @returns Nenhum valor é retornado, mas os dados do clima são atribuídos à propriedade weatherDatas do componente.
    */
   getWeatherDatas(cityName: string): void {
-    this.weatherService.getWeatherDatas(cityName).subscribe({
-      next: (response) => {
-        response && (this.weatherDatas = response);
-        console.log(this.weatherDatas);
-      },
-      error: (error) => {
-        console.log(error);
-      },
-    });
+    this.weatherService
+      .getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherDatas = response);
+          console.log(this.weatherDatas);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+
+  /**
+   * Envia o nome da cidade para obter os dados do clima e limpa o campo de entrada.
+   */
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
+  }
+
+  /**
+   * Método que é chamado quando o componente é destruído.
+   * Ele emite um valor para o subject `destroy$` e completa o subject.
+   * @returns void
+   */
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
